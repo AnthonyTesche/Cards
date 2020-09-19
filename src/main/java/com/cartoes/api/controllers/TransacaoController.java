9396e98693f6962fd5371bdd2a78ebcc8f8a77a3
Anthony Tesche <tescheanthony@gmail.com>
2020-09-19 08:20:50 -0300

    :fire: Removido imports não utilizados

--- src/main/java/com/cartoes/api/controllers/TransacaoController.java
+++ src/main/java/com/cartoes/api/controllers/TransacaoController.java
@@ -1,7 +1,6 @@
 package com.cartoes.api.controllers;
  
 import java.util.List;
-import java.util.Optional;
  
 import javax.validation.Valid;
  
@@ -11,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,9 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cartoes.api.dtos.CartaoDto;
 import com.cartoes.api.dtos.TransacaoDto;
-import com.cartoes.api.entities.Cartao;
 import com.cartoes.api.entities.Transacao;
 import com.cartoes.api.services.TransacaoService;
 import com.cartoes.api.utils.ConsistenciaException;
