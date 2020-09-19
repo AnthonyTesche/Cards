7f83ec1d868a5e250117c26118462f9705089b91
Anthony Tesche <tescheanthony@gmail.com>
2020-09-19 08:37:05 -0300

    :art: Formatado Documento

--- src/main/java/com/cartoes/api/utils/ConversaoUtils.java
+++ src/main/java/com/cartoes/api/utils/ConversaoUtils.java
@@ -1,20 +1,20 @@
 package com.cartoes.api.utils;
- 
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
- 
+
 import com.cartoes.api.dtos.CartaoDto;
 import com.cartoes.api.dtos.ClienteDto;
 import com.cartoes.api.dtos.TransacaoDto;
 import com.cartoes.api.entities.Cartao;
 import com.cartoes.api.entities.Cliente;
 import com.cartoes.api.entities.Transacao;
- 
+
 public class ConversaoUtils {
- 
-   	public static Cartao Converter(CartaoDto cartaoDto) throws ParseException {
+
+	public static Cartao Converter(CartaoDto cartaoDto) throws ParseException {
 		Cartao cartao = new Cartao();
 		if (cartaoDto.getId() != null && cartaoDto.getId() != "")
 			cartao.setId(Integer.parseInt(cartaoDto.getId()));
@@ -25,8 +25,8 @@ public class ConversaoUtils {
 		cliente.setId(Integer.parseInt(cartaoDto.getClienteId()));
 		cartao.setCliente(cliente);
 		return cartao;
-   	}
-   	
+	}
+
 	public static CartaoDto Converter(Cartao cartao) {
 		CartaoDto cartaoDto = new CartaoDto();
 		cartaoDto.setId(String.valueOf(cartao.getId()));
@@ -35,25 +35,25 @@ public class ConversaoUtils {
 		cartaoDto.setBloqueado(String.valueOf(cartao.getBloqueado()));
 		cartaoDto.setClienteId(String.valueOf(cartao.getCliente().getId()));
 		return cartaoDto;
-   	}
-   	
-   	public static List<CartaoDto> ConverterLista(List<Cartao> list){
+	}
+
+	public static List<CartaoDto> ConverterLista(List<Cartao> list) {
 		List<CartaoDto> lst = new ArrayList<CartaoDto>(list.size());
 		for (Cartao card : list) {
-				lst.add(Converter(card));
+			lst.add(Converter(card));
 		}
 		return lst;
-   	}
-   	
-   	public static List<TransacaoDto> ConvertListTrans(List<Transacao> list){
+	}
+
+	public static List<TransacaoDto> ConvertListTrans(List<Transacao> list) {
 		List<TransacaoDto> lst = new ArrayList<TransacaoDto>(list.size());
 		for (Transacao tr : list) {
-				lst.add(Converter(tr));
+			lst.add(Converter(tr));
 		}
 		return lst;
-   	}
- 
-   	public static Cliente Converter(ClienteDto clienteDto) {
+	}
+
+	public static Cliente Converter(ClienteDto clienteDto) {
 		Cliente cliente = new Cliente();
 		if (clienteDto.getId() != null && clienteDto.getId() != "")
 			cliente.setId(Integer.parseInt(clienteDto.getId()));
@@ -61,9 +61,9 @@ public class ConversaoUtils {
 		cliente.setCpf(clienteDto.getCpf());
 		cliente.setUf(clienteDto.getUf());
 		return cliente;
-   	}
-    
-   	public static Transacao Converter(TransacaoDto transacaoDto) {
+	}
+
+	public static Transacao Converter(TransacaoDto transacaoDto) {
 		Transacao transacao = new Transacao();
 		if (transacaoDto.getId() != null && transacaoDto.getId() != "")
 			transacao.setId(Integer.parseInt(transacaoDto.getId()));
@@ -73,25 +73,25 @@ public class ConversaoUtils {
 		transacao.setCnpj(transacaoDto.getCnpj());
 		transacao.setCartao(transacaoDto.getCartao());
 		return transacao;
-   	}
- 
-   	public static TransacaoDto Converter(Transacao transacao) {
-   		TransacaoDto transacaoDto = new TransacaoDto();
-     	transacaoDto.setId(String.valueOf(transacao.getId()));
-     	transacaoDto.setJuros(String.valueOf(transacao.getJuros()));
-     	transacaoDto.setQdtParcelas(String.valueOf(transacao.getQdtParcelas()));
-     	transacaoDto.setValue(String.valueOf(transacao.getValue()));
-     	transacaoDto.setCartao(transacao.getCartao());
-     	transacaoDto.setCnpj(transacao.getCnpj());
-     	return transacaoDto;
-   	}
-   	
-   	public static ClienteDto Converter(Cliente cliente) {
+	}
+
+	public static TransacaoDto Converter(Transacao transacao) {
+		TransacaoDto transacaoDto = new TransacaoDto();
+		transacaoDto.setId(String.valueOf(transacao.getId()));
+		transacaoDto.setJuros(String.valueOf(transacao.getJuros()));
+		transacaoDto.setQdtParcelas(String.valueOf(transacao.getQdtParcelas()));
+		transacaoDto.setValue(String.valueOf(transacao.getValue()));
+		transacaoDto.setCartao(transacao.getCartao());
+		transacaoDto.setCnpj(transacao.getCnpj());
+		return transacaoDto;
+	}
+
+	public static ClienteDto Converter(Cliente cliente) {
 		ClienteDto clienteDto = new ClienteDto();
 		clienteDto.setId(String.valueOf(cliente.getId()));
 		clienteDto.setNome(cliente.getNome());
 		clienteDto.setCpf(cliente.getCpf());
 		clienteDto.setUf(cliente.getUf());
 		return clienteDto;
-   	}
+	}
 }
